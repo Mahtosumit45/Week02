80081ae8d49d96df05a6b1dd859295320b8a046b
sumit kumar mahto <kumarmahtosumit45@gmail.com>
2025-01-17 11:55:59 +0530

    updated

--- Day01/lev2/StudentClass.java
+++ Day01/lev2/StudentClass.java
@@ -1,10 +1,4 @@
-//Program to Simulate Student Report
-//Problem Statement: Create a Student class with attributes name, rollNumber, and marks. Add two methods:
-//To calculate the grade based on the marks.
-//To display the student's details and grade.
-//Explanation: The Student class organizes all relevant details about a student as attributes. Methods are used to calculate the grade and provide a way to display all information.
 
-//write code here....
 import java.util.Scanner;
 public class StudentClass {
 	public static void main(String[] args){
